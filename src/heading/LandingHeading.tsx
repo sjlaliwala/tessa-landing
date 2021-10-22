9d4d011dcf696d135b2a9138ed3c3165aea3f2e0
Srini Laliwala <sjlaliwala246@gmail.com>
2021-10-22 02:45:05 -0400

    change color of title

--- src/heading/LandingHeading.tsx
+++ src/heading/LandingHeading.tsx
@@ -2,13 +2,10 @@ const LandingHeading = () => (
   <header className="justify-center">
     <div className="mb-7">
       <h1 className="text-5xl text-gray-900 font-bold inline-block">
-        {'Intelligently '}
-        <h1 className="text-5xl text-blue-500 font-bold inline-block">
-          {'Fast Tracking'}
-        </h1>
+        {'Intelligently Fast Tracking Your'}
       </h1>
-      <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line inline-block">
-        {'Your Professional Development'}
+      <h1 className="text-5xl text-blue-500 font-bold whitespace-pre-line inline-block">
+        {'Professional Development'}
       </h1>
     </div>
 
