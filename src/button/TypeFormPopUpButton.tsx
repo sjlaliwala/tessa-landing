acd47192154bb5ab9b5a04f1a363849d90988bd1
Srini L <sjlaliwala246@gmail.com>
2021-11-14 16:05:21 -0500

    change typeform to new one

--- src/button/TypeFormPopUpButton.tsx
+++ src/button/TypeFormPopUpButton.tsx
@@ -7,7 +7,7 @@ type ITypeFormButton = {
 const TypeFormPopUpButton = (props: ITypeFormButton) => {
   return (
     <PopupButton
-      id="HMTl6pl6"
+      id="FBcXTJHx"
       className="h-12 px-8 text-white text-2xl shadow p-2 rounded bg-blue-500 hover:bg-blue-400 focus:shadow-outline"
     >
       <h3 className="text-xl font-semibold">{props.title}</h3>
